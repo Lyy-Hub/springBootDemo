2688008e650d8b5d8b19b8825cbee763b22eaf25
LiYueYang <13953193651@163.com>
2020-01-09 15:04:47 +0800

    引入 logback 日志，Test类中做测试，暂未加入具体业务代码中

--- src/main/java/com/lyy/Test.java
+++ src/main/java/com/lyy/Test.java
@@ -1,15 +1,22 @@
 package com.lyy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 
 /**
  * Created by liYueYang on 2019/9/4.
  */
 public class Test {
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
     public static void main(String[] args) {
-        String strPassword ="Unknown";
-        char [] charPassword = new char [] {'U','n','k','w','o','n'};
-        System.out.println("字符密码："+ strPassword);
-        System.out.println("字符密码："+ Arrays.toString(charPassword));
+        String strPassword = "Unknown";
+        char[] charPassword = new char[]{'U', 'n', 'k', 'w', 'o', 'n'};
+        System.out.println("字符密码：" + strPassword);
+        System.out.println("字符密码：" + Arrays.toString(charPassword));
+        logger.error("字符密码：" + strPassword);
+        logger.error("字符密码：" + Arrays.toString(charPassword));
     }
 }
